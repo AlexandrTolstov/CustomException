1c49282cf5cccf911b5df6e7ec1b561ebbcaac72
Alexandr <tolstov.alexandr.85@gmail.com>
2019-06-13 09:38:43 +0600

    1

--- CustomException/Program.cs
+++ CustomException/Program.cs
@@ -10,6 +10,19 @@ namespace CustomException
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Custom Exception ******\n");
+            Car myCar = new Car("Rusty", 90);
+            try
+            {
+                myCar.Accelerate(50);
+            }
+            catch (CarIsDeadException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ErrorTimeStamp);
+                Console.WriteLine(e.CauseOfError);
+            }
+            Console.ReadLine();
         }
     }
 }
